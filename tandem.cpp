404201d02a7682dcd94b0d899f35d98696d3b853
Natalie Tasman <natalie.tasman@insilicos.com>
2012-01-19 13:31:46 -0800

    Adding parameter to regression test commandline to specify build directory, for example on non-MSVC builds

--- tandem.cpp
+++ tandem.cpp
@@ -610,7 +610,14 @@ int main(int argc, char* argv[])
 	string strValue;
 	pProcess[0]->m_xmlValues.get(strKey,strValue); // ex. strValue="foo.tandem"
         // run Tandem2XML "foo.tandem" > "foo.tandem.pepXML"
+
+	string builddir = "";
+	if (argc>3) { // use a build directory
+	  builddir = argv[3];
+	  cout << "(finding TPP executables in " << builddir << ")" << endl;
+	}
         string cmd("Tandem2XML \"");
+	cmd = builddir + cmd; // build directory prefix
         cmd += strValue;
         cmd += "\" > \"";
         string pepxml(strValue);
@@ -620,6 +627,7 @@ int main(int argc, char* argv[])
         system(cmd.c_str());
         // run InteractParser -t[!]"foo.tandem.test" "foo.tandem.interact.pepXML" "foo.tandem.pepXML" 
         cmd = "InteractParser ";
+	cmd = builddir + cmd;  // build directory prefix
         cmd += argv[2]; 
         cmd += "\"";
         cmd += strValue;
